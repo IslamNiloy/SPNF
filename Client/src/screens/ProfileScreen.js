66a7e37b02c91671cd35397b023b3e3aa32e7dc7
mashrurhossainkhan <khanmashrur03@gmail.com>
2024-08-22 19:33:56 +0600

    removing insertion in subscription

--- Client/src/screens/ProfileScreen.js
+++ Client/src/screens/ProfileScreen.js
@@ -1,10 +1,24 @@
 import Footer from '../components/Footer/Footer';
 import Header from '../components/header/Header';
 import UserProfile from '../components/UserProfile/userProfile';
-
+import React, { useEffect } from 'react';
+import { useDispatch } from "react-redux";
+import { userInfoByID } from '../action/userAction';
+import { useCookies } from 'react-cookie'
+import { useNavigate } from 'react-router-dom';
+import { insertIntoSubscriptionCollection } from '../action/subscriptionAction';
 
 const ProfileScreen = () => {
-
+    const [cookies, setCookie] = useCookies(['subscription']);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    useEffect(() => {
+      const portalID = localStorage.getItem('I8PD56?#C|NXhSgZ0KE');
+      if(portalID){
+        dispatch(userInfoByID(portalID));
+        //dispatch(insertIntoSubscriptionCollection(portalID,cookies.subscription));
+      } 
+  }, [dispatch]);
     return(
         <div>
             <Header />
