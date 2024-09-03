f0efdbd4b5ee64e8cf5caa37607f9e0d80710ed2
mashrurhossainkhan <khanmashrur03@gmail.com>
2024-09-03 20:28:49 +0600

    adding corsOption

--- Server/index.js
+++ Server/index.js
@@ -17,12 +17,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Use CORS middleware
-app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+const corsOptions ={
+  origin:'https://dev-frontend-hpu-phone-number-fortmatter.vercel.app', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 /* 
   STRIPE WEBHOOK MUST BE HANDLED BEFORE ANY JSON{} DATA IS PARSED FROM SERVER 
 */
