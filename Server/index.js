6d1bf1b60a7440fbae5b715a4ac033402d0b74a4
mashrurhossainkhan <khanmashrur03@gmail.com>
2024-09-03 19:47:17 +0600

    adding cors 3

--- Server/index.js
+++ Server/index.js
@@ -18,6 +18,11 @@ const PORT = process.env.PORT || 3000;
 
 // Use CORS middleware
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 /* 
   STRIPE WEBHOOK MUST BE HANDLED BEFORE ANY JSON{} DATA IS PARSED FROM SERVER 
 */
