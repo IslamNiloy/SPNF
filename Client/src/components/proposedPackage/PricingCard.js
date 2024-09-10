a79a06c6bfbfcf60f828de8047a77e0e92db12c9
mashrurhossainkhan <khanmashrur03@gmail.com>
2024-09-10 19:07:40 +0600

    pricing card design - logic implementation

--- Client/src/components/proposedPackage/PricingCard.js
+++ Client/src/components/proposedPackage/PricingCard.js
@@ -1,12 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import {Link, useNavigate } from 'react-router-dom';
 import { allPackages } from '../../action/packageAction';
+import { BackendAPI } from "../../api/server";
 import LoadingBox from '../LoadingBox';
 import MessageBox from '../MessageBox';
 import './PricingCards.css'; // Import the CSS file
+import { subscriptionInfoByID } from "../../action/subscriptionAction";
+import { paymentInfoByEmail } from "../../action/paymentAction";
 
 const PricingCard = ({ planName, monthlyPrice, yearlyPrice, limit, countries, buttonText, isPopular, isChosen, isMonthly }) => {
+  const today = new Date();
+  const portalID = localStorage.getItem('I8PD56?#C|NXhSgZ0KE');
+  const [old_price, setOld_Price] = useState("");
+  const [apiCallCount, setAPICallCount] = useState("");
+  const [packageName, setPackageName] = useState("");
+  const [price, setPrice] = useState("");
+  const [status, setStatus] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const Subscription = useSelector((state) => state.getSubscriptionByID);
+  const { loading, error, infos } = Subscription;
+
+  const paymentInfoFromAction = useSelector((state) => state.paymentInfoByEmail);
+  const { loading : paymentLoading, error: paymentErr, paymentInfo } = paymentInfoFromAction;
+   
+  const dispatch = useDispatch();
+  const allPackageInfo = useMemo(() => JSON.parse(localStorage.getItem('lSYs~K@jx}DS1YG>/57Kuj')), []);
+
+  useEffect(() => {
+    if (!infos) {
+      const emailForPaymentInfo = localStorage.getItem("spPhk44lI519pJ");
+      dispatch(subscriptionInfoByID(portalID));
+      dispatch(paymentInfoByEmail(emailForPaymentInfo));
+  }
+  if(infos){
+      const filteredPackages = allPackageInfo.filter(packages => packages._id === infos.package);
+      setOld_Price(filteredPackages[0].price);
+      setPackageName(filteredPackages[0].packageName);
+      setPrice(filteredPackages[0].price);
+      setAPICallCount(infos.apiCallCount);
+      setEndDate(new Date(infos.packageEndDate));
+      if(paymentInfo)
+        setStatus(paymentInfo.status);
+  }
+}, [dispatch, infos, portalID, allPackageInfo]);
+
   return (
     <div className={`pricing-card ${isPopular ? 'most-popular' : ''}`}>
       <div className="plan-header">
@@ -23,6 +61,62 @@ const PricingCard = ({ planName, monthlyPrice, yearlyPrice, limit, countries, bu
         <li>✔ Actions {limit}</li>
         <li>✔ {countries}</li>
       </ul>
+
+
+
+
+
+
+
+      {
+        (endDate!="" && today > endDate && 
+          planName=="Free" && 
+          planName != "Installation Package")?
+        ( 
+          <Link to='/profile'>
+              <Link to='/profile'>
+              <button className={`plan-button ${isChosen ? 'chosen' : ''}`}>
+                  upgrade plan
+              </button>
+            </Link>
+          </Link>
+  
+        ):
+        (planName=="Free" 
+          && planName == "Free" 
+          && planName != "Installation Package") ||
+          (status != 'cancelled' 
+            && planName == "Enterprise" 
+            && planName != "Installation Package") ||
+          (status != 'cancelled' 
+            && (planName=="Free" || planName=="Pro") 
+            && planName == "Pro" && planName != "Installation Package") ||
+          (apiCallCount >= 500 && planName=="Free" 
+            && status == 'cancelled' && planName != "Installation Package")
+          ?
+          (
+            <Link to='/profile'>
+            <button type="submit" className="install-button">
+                upgrade plan
+            </button>
+          </Link>
+        ):""
+
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       <button className={`plan-button ${isChosen ? 'chosen' : ''}`}>
         {buttonText}
       </button>
