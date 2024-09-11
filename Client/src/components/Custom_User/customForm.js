96968f362b74d826e8bf8638b72c3cf6431d8a1a
tonmoy5 <tanzinulkabir@gmail.com>
2024-09-11 20:26:43 +0600

    Custom form

--- Client/src/components/Custom_User/customForm.js
+++ Client/src/components/Custom_User/customForm.js
@@ -1,10 +1,10 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import './customForm.css';
 
 const CustomUser = () => {
     useEffect(() => {
         const script = document.createElement('script');
-        script.src='https://js.hsforms.net/forms/embed/v2.js';
+        script.src = 'https://js.hsforms.net/forms/embed/v2.js';
         document.body.appendChild(script);
 
         script.addEventListener('load', () => {
@@ -23,6 +23,8 @@ const CustomUser = () => {
 
     return (
         <div className="hubspotForm_main">
+            <h2 className="workflow-title">Get <span>Custom Subscription</span></h2>
+            <p>Please submit this form for custom package, one of our team member will reach out to you ASAP</p>
             <div id="hubspotForm"></div>
         </div>
     );
--- Client/src/screens/CustomUserScreen.js
+++ Client/src/screens/CustomUserScreen.js
@@ -5,13 +5,17 @@ import Header from '../components/header/Header';
 import OverviewSection from '../components/overview/OverviewSection';
 import ProposedPackageSection from '../components/proposedPackage/ProposedPackageSection';
 import UsingTheAppSection from '../components/UsingTheAppSection/UsingTheAppSection';
+import PricingPlan from '../components/proposedPackage/PricingCard';
+import FeaturesAndBenefits from '../components/FeaturesAndBenefits/FeaturesAndBenefits';
 
 const CustomUserScreen = () => {
-    return(
+    return (
         <div>
             <Header />
-            <CustomUser/>
-            <Footer/>
+            <CustomUser />
+            <FeaturesAndBenefits />
+            <PricingPlan />
+            <Footer />
         </div>
     )
 }
