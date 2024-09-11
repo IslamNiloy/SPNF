16596888561e8e237c38d0f03c92c349b7bfb373
tonmoy5 <tanzinulkabir@gmail.com>
2024-09-11 17:37:19 +0600

    Hero button design and link changed

--- Client/src/components/heroSection/HeroSection.js
+++ Client/src/components/heroSection/HeroSection.js
@@ -1,6 +1,6 @@
 // src/HeroSection.js
 import React from 'react';
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import './HeroSection.css';
 import { useCookies } from 'react-cookie'
 import { Link } from 'react-router-dom';
@@ -10,7 +10,7 @@ import { BackendAPI } from "../../api/server";
 const HeroSection = () => {
     const userDetails = useSelector((state) => state.userInfo);
     const { loading, error, userInfo } = userDetails;
-    const localUserInfo =  localStorage.getItem('I8PD56?#C|NXhSgZ0KE');
+    const localUserInfo = localStorage.getItem('I8PD56?#C|NXhSgZ0KE');
     const [cookies, setCookie] = useCookies(['subscription']);
 
 
@@ -18,23 +18,21 @@ const HeroSection = () => {
     return (
         <div>
             <section className="hero-section">
-            <div className="hero-content">
-                <button className="btn hero-btn1">
-                    HubSpot Phone Number Formatter
-                </button>
-                <h1>Format Phone Numbers <span style={{color: "#000000"}}>with Ease</span></h1>
-                <p>Automatically format phone numbers from any country, with a simple click.</p>
-                <div className="buttons">
-                    <ScrollLink to="features">
+                <div className="hero-content">
+                    <button className="btn hero-btn1">
+                        HubSpot Phone Number Formatter
+                    </button>
+                    <h1>Format Phone Numbers <span style={{ color: "#000000" }}>with Ease</span></h1>
+                    <p>Automatically format phone numbers from any country, with a simple click.</p>
+                    <div className="buttons">
                         <Link to={`${BackendAPI}/install`}>
                             <button className="btn hero-btn2">
                                 Get Your Formatter Now!
                             </button>
                         </Link>
-                    </ScrollLink>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
         </div>
     );
 };
