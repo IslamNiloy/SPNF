07c8698a9dd526769f9a46e052da8dea574a5573
tonmoy5 <tanzinulkabir@gmail.com>
2024-09-20 14:28:01 +0600

    QualityCheck section

--- Client/src/components/QualityCheckSection/QualityCheckSection.js
+++ Client/src/components/QualityCheckSection/QualityCheckSection.js
@@ -6,16 +6,19 @@ const QualityCheckSection = () => {
     <section className="quality-check-container">
       {/* Left side image */}
       <div className="image-wrapper_qc">
-      <div className="image-back_qc">
-        <img src="/WorkflowSection/background.png" alt="Background Image" />
+        <div className="image-back_qc">
+          <img src="/WorkflowSection/background.png" alt="Background Image" />
+        </div>
+        <div className="image-front_qc">
+          <img src="/QualityCheck/image.png" alt="Background Image" />
+        </div>
       </div>
-      <div className="image-front_qc">
-        <img src="/QualityCheck/image.png" alt="Background Image" />
-      </div>
-      </div> 
 
       {/* Right side content */}
       <div className="quality-check-content">
+        <button className="btn hero-btn1">
+          Gain Insights into Your Data Integrity
+        </button>
         <h2 className="quality-check-title">
           Phone Number <span>Quality Check</span>
         </h2>
@@ -26,10 +29,10 @@ const QualityCheckSection = () => {
         <div className="quality-check-features">
           <h3>Comprehensive Error Detection</h3>
           <p>Automatically generate reports that identify phone numbers with incorrect or missing country codes, invalid formats, and other common issues.</p>
-
+          <hr />
           <h3>Data Quality Insights</h3>
           <p>Get a clear overview of your data's health with visual representations of common misformatting issues, helping you take corrective action.</p>
-
+          <hr />
           <h3>Actionable Intelligence</h3>
           <p>Use these reports to refine your data entry processes, update training for your team, or even adjust your CRM workflows to minimize future errors.</p>
         </div>
