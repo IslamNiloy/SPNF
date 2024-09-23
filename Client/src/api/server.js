26ff4443cbe578c35dc56cf86fa87a3e6b873c49
mashrurhossainkhan <khanmashrur03@gmail.com>
2024-09-23 14:56:22 +0600

    publish update

--- Client/src/api/server.js
+++ Client/src/api/server.js
@@ -1,5 +1,3 @@
 export const BackendAPI = 'https://app.formatphonenumber.com';
 
 //export const BackendAPI = 'http://localhost:3003';
-
-//export const BackendAPI = 'https://dev-hpu-phonenumber-fortmatter.vercel.app'
