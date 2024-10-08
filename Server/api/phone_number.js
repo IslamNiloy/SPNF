57593c5f47a17ce13815d753a16b4a0bbafa34a2
mashrurhossainkhan <khanmashrur03@gmail.com>
2024-10-08 21:40:24 +0600

    changing cron files

---
+++ Server/api/phone_number.js
@@ -0,0 +1,16 @@
+const { bulkPhoneNumberApiCallCount } = require("../controllers/Logic/packageConditions");
+
+module.exports = async function handler(req, res) {
+  if (req.headers.authorization !== `Bearer ${process.env.CRON_SECRET}`) {
+    return res.status(401).end('Unauthorized');
+  }
+
+  try {
+    console.log('bulkPhoneNumberApiCallCount');
+    await bulkPhoneNumberApiCallCount();
+    res.status(200).end('bulkPhoneNumberApiCallCount successfully');
+  } catch (error) {
+    console.error('Error bulkPhoneNumberApiCallCount:', error);
+    res.status(500).end('Failed to bulkPhoneNumberApiCallCount');
+  }
+};
