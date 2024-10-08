67974645cab14801361ea6d5b8cf1a6b26689ffa
mashrurhossainkhan <khanmashrur03@gmail.com>
2024-10-08 21:14:28 +0600

    edit cron files

--- Server/api/check_phone_number.js
+++ Server/api/check_phone_number.js
@@ -1,19 +1,11 @@
-import axios from 'axios';
+import { bulk_Check_PhoneNumberApiCallCount } from '../controllers/Logic/packageConditions';
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {  // This will be called by Vercel cron as a GET request
-    try {
-      // Make an internal PUT request to your backend
-      const response = await axios.put(`${process.env.BACKEND_URL}/format/bulk/update/check_phone_number`);
-
-      // Return the result
-      res.status(200).json({ message: 'PUT request to /format/bulk/update/check_phone_number successful', data: response.data });
-    } catch (error) {
-      console.error('Error in PUT request to /format/bulk/update/check_phone_number:', error);
-      res.status(500).json({ error: 'Failed to make PUT request to /format/bulk/update/check_phone_number' });
-    }
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+  try {
+    bulk_Check_PhoneNumberApiCallCount();
+    res.status(200).json({ message: 'PUT request to /format/bulk/update/check_phone_number successful', data: response.data });
+  } catch (error) {
+    console.error('Error in /format/bulk/update/check_phone_number:', error);
+    res.status(500).json({ error: 'Failed to make PUT request to /format/bulk/update/check_phone_number' });
   }
 }
--- Server/api/remove_cache.js
+++ Server/api/remove_cache.js
@@ -1,16 +1,11 @@
-import axios from 'axios';
+import { removeAllCache } from '../controllers/dataFormatterController';
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    try {
-      const response = await axios.put(`${process.env.BACKEND_URL}/format/remove/cache/all`);
-      res.status(200).json({ message: 'PUT request to /format/remove/cache/all successful', data: response.data });
-    } catch (error) {
-      console.error('Error in PUT request to /format/remove/cache/all:', error);
-      res.status(500).json({ error: 'Failed to make PUT request to /format/remove/cache/all' });
-    }
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+  try {
+    removeAllCache();
+    res.status(200).json({ message: 'PUT request to /format/remove/cache/all successful', data: response.data });
+  } catch (error) {
+    console.error('Error in /format/remove/cache/all:', error);
+    res.status(500).json({ error: 'Failed to make PUT request to /format/remove/cache/all' });
   }
 }
