328a6e898b0d90fc91386aed9e4018eb0d8f7435
mashrurhossainkhan <khanmashrur03@gmail.com>
2024-10-23 14:31:26 +0600

    merging main and dev for all pricing plan

--- Client/src/App.js
+++ Client/src/App.js
@@ -8,20 +8,23 @@ import WelcomeScreen from './screens/WelcomeScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import CustomUserScreen from './screens/CustomUserScreen';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import PricingScreen from './screens/PricingScreen';
 
 // Your Stripe public key (get this from your Stripe dashboard)
 //const stripePromise = loadStripe('pk_test_51PgQiFC39bb601vJn7f3Hu7A1UrcDwUXodNoxy8rL8YY2AbyIuJ0HH0xAl5p4JCEZlSCutglnGOr3bzVlu2hAYlJ00kIM8Tr2a');
 
 function App() {
-  //const portalID = localStorage.getItem('I8PD56?#C|NXhSgZ0KE');
+  const portalID = localStorage.getItem('I8PD56?#C|NXhSgZ0KE');
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomeScreen />} exact/>
-        <Route path="/welcome" element={<WelcomeScreen />} exact/>
+      
+        {portalID &&   <Route path="/welcome" element={<WelcomeScreen />} exact/>}
+        <Route path="/all-pricing-plans" element={<PricingScreen />} exact/>
         {/*<Route path="/payment" element={<PaymentScreen />} exact/>*/}
-       
-        <Route path="/profile" element={<ProfileScreen />} exact/>
+       {portalID && <Route path="/profile" element={<ProfileScreen />} exact/>}
+  
         
         <Route path="/custom" element={<CustomUserScreen/>} exact/>
       </Routes>
