49d36cf223aa8e0935d63653872f2cc446c96677
mashrurhossainkhan <khanmashrur03@gmail.com>
2024-10-30 19:38:21 +0600

    chaning mongoose

--- Server/utils/mongo.connection.js
+++ Server/utils/mongo.connection.js
@@ -3,19 +3,37 @@ require("dotenv").config();
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
+const options = {
+    maxPoolSize: 20,                  // Maximum number of connections in the pool
+    minPoolSize: 5,                   // Minimum number of connections in the pool
+    serverSelectionTimeoutMS: 5000,   // Timeout for initial server selection
+    socketTimeoutMS: 45000,           // Closes inactive sockets after 45 seconds
+    connectTimeoutMS: 30000,          // Timeout for initial connection attempts
+  };
+
 mongoose.connection.once("open", () => {
-   //TODO:insert in package
     console.log(`Mongodb Connection is ready..🚀🚀`);
 });
 mongoose.connection.on("error", (err) => {
-    console.log(err);
+    console.log("Database connection error:", err);
 });
 
 async function ConnectDB() {
-    await mongoose.connect(DATABASE_URL);
+    try {
+        await mongoose.connect(DATABASE_URL, options);
+        console.log("Connected to the database");
+    } catch (err) {
+        console.error("Error connecting to the database:", err);
+    }
 }
+
 async function disconnectDB() {
-    await mongoose.disconnect();
+    try {
+        await mongoose.disconnect();
+        console.log("Disconnected from the database");
+    } catch (err) {
+        console.error("Error disconnecting from the database:", err);
+    }
 }
 
 module.exports = {
